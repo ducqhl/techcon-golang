f44ba8f3e70c443fcb20c8c10735c7f4a4de4f7b
Duc.Le2 <Duc.Le2@coxautoinc.com>
2019-09-26 23:18:59 +0700

    add demo prime th

--- go/fibonance.go
+++ go/fibonance.go
@@ -1,19 +1,28 @@
-package fibonance
+var url = require('url');
 
-// Calculate Fibonance
-func Fibonance(n int) int {
-	f1, f2 := 1, 1
-    if (n == 1 || n == 2)
-		return 1;
-		
-	num := 0
+const fibonance = (n) => {
+    a1 = 1;
+    a2 = 1;
 
-    for i := 3; i <= n; i++
-    {
-        num = f1 + f2;
-        f1 = a2;
-        f2 = num;
-	}
-	
-    return num;
-}
+    if (n == 1 || n == 2) return 1;
+
+    a = 0;
+
+    for (let i = 3; i <= n; ++i) {
+        a = a1 + a2;
+        a1 = a2;
+        a2 = a;
+    }
+
+    return a;
+};
+
+const fibonanceController = (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    query = url.parse(req.url, true).query;
+    result = fibonance(query.n);
+    res.write(result.toString());
+    res.end();
+};
+
+module.exports = fibonanceController;
--- node/index.js
+++ node/index.js
@@ -1,15 +1,23 @@
 const http = require('http');
 const fibonanceController = require('./fibonance')
 const readFileController = require('./read-file')
+const nthPrimeController = require('./nthprime')
 
 const handler = (req, res) => {
+    console.log(`${new Date()} - ${req.method} - ${req.url}`)
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
+
     if(req.url.startsWith("/fibonance")){
         return fibonanceController(req, res);
     }
 
     if(req.url.startsWith("/readfile")){
         return readFileController(req, res);
+    }
+
+    if(req.url.startsWith("/nthprime")){
+        return nthPrimeController(req, res);
     }
 
     res.write('hit success');
