9c129913a463aea35e9d85eb733753879e823f02
Duc.Le2 <Duc.Le2@coxautoinc.com>
2019-10-01 20:56:02 +0700

    update api server route

--- go/index.go
+++ go/index.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 )
 
@@ -19,26 +18,6 @@ func hitMeHandler(w http.ResponseWriter, r *http.Request) {
 func sortHandler(w http.ResponseWriter, r *http.Request) {
 	array := sort1bilionTimes()
 	fmt.Fprintf(w, "%v", array)
-}
-
-func fibonacciHandler(w http.ResponseWriter, r *http.Request) {
-	//https://planetmath.org/listoffibonaccinumbers
-	params := r.URL.Query()
-	n := params.Get("n")
-	nInt, _ := strconv.ParseInt(n, 0, 64)
-	result := fibonacci(nInt)
-
-	fmt.Fprintf(w, "%v", result)
-}
-
-func primethHandler(w http.ResponseWriter, r *http.Request) {
-	//https://primes.utm.edu/lists/small/1000.txt
-	params := r.URL.Query()
-	n := params.Get("n")
-	nInt, _ := strconv.ParseInt(n, 0, 64)
-	result := getNthPrime(nInt)
-
-	fmt.Fprintf(w, "%d", result)
 }
 
 func readFileHandler(w http.ResponseWriter, r *http.Request) {
@@ -58,9 +37,7 @@ func readFileHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	httpPort := 8081
 
-	http.HandleFunc("/fibonacci", fibonacciHandler)
 	http.HandleFunc("/hitme", hitMeHandler)
-	http.HandleFunc("/nthprime", primethHandler)
 	http.HandleFunc("/readfile", readFileHandler)
 	http.HandleFunc("/sort", sortHandler)
 
@@ -75,52 +52,6 @@ func main() {
 /*********************************************
 *               Helper Functions
 **********************************************/
-
-func fibonacci(n int64) int64 {
-	var f1, f2, nth int64 = 0, 1, 0
-
-	for i := int64(0); i <= n; i++ {
-		nth = f1 + f2
-		f1 = f2
-		f2 = nth
-	}
-
-	return nth
-}
-
-// prime check
-func isPrime(number int64) bool {
-
-	if number < int64(2) {
-		return false
-	}
-
-	for i := int64(2); i < number; i++ {
-		if number%i == 0 {
-			return false
-		}
-	}
-
-	return true
-}
-
-//get nth prime
-func getNthPrime(nth int64) int64 {
-	if nth < 1 {
-		return -1
-	}
-
-	var nthPrime, prime int64 = 0, -1
-	for i := int64(2); nthPrime < nth; i++ {
-		if isPrime(i) {
-			prime = i
-			nthPrime++
-		}
-	}
-
-	return prime
-}
-
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now()
--- node/index.js
+++ node/index.js
@@ -1,35 +1,25 @@
 const http = require('http');
-const hitMeController = require('./hitme')
-const fibonacciController = require('./fibonacci')
-const readFileController = require('./read-file')
-const sortController = require('./sort')
-const whileTrueController = require('./whiletrue')
+const hitMeController = require('./hitme');
+const readFileController = require('./read-file');
+const sortController = require('./sort');
 
 const handler = (req, res) => {
-    console.log(`Node: ${new Date().toLocaleTimeString()} - ${req.method} - ${req.url}`)
+    console.log(`Node: ${new Date().toLocaleTimeString()} - ${req.method} - ${req.url}`);
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    if(req.url.startsWith("/hitme")){
+    if (req.url.startsWith('/hitme')) {
         return hitMeController(req, res);
     }
 
-    if(req.url.startsWith("/fibonacci")){
-        return fibonacciController(req, res);
-    }
-
-    if(req.url.startsWith("/readfile")){
+    if (req.url.startsWith('/readfile')) {
         return readFileController(req, res);
     }
 
-    if(req.url.startsWith("/sort")){
+    if (req.url.startsWith('/sort')) {
         return sortController(req, res);
     }
 
-    if(req.url.startsWith("/whiletrue")){
-        return whileTrueController(req, res);
-    }
-    
     res.write('hit success');
     res.end();
 };
